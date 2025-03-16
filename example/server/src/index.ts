21cb801b532dc8b806a06123426c2e7e6b013b9b
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2025-03-16 14:21:44 +0900

    implement simple SSE endpoint

--- example/server/src/index.ts
+++ example/server/src/index.ts
@@ -1,10 +1,27 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { SSEStreamingApi, streamSSE } from 'hono/streaming'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/', async (c) => {
+  const body = await c.req.json()
+  const message = `Hello, ${body.name}!`
+
+  const handleStream = async (stream: SSEStreamingApi) => {
+    let joinedMessage = ''
+    for (const char of message) {
+      joinedMessage += char
+      await stream.writeSSE({
+        data: JSON.stringify({
+          message: char,
+          joinedMessage,
+        }),
+      })
+    }
+  }
+
+  return streamSSE(c, handleStream)
 })
 
 serve({
